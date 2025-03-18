6ea4a4152eeb8c4d34d23ac38da508a49528a73c
andy12341210 <andy12341210@gmail.com>
2025-03-18 21:36:00 +0800

    tested model

--- student_agent.py
+++ student_agent.py
@@ -3,15 +3,17 @@ import numpy as np
 import pickle
 import random
 import gym
+with open("taxi_dqn_model.pkl", "rb") as f:
+ Q_table = pickle.load(f)
+
+
 
 def get_action(obs):
     
-    # TODO: Train your own agent
-    # HINT: If you're using a Q-table, consider designing a custom key based on `obs` to store useful information.
-    # NOTE: Keep in mind that your Q-table may not cover all possible states in the testing environment.
-    #       To prevent crashes, implement a fallback strategy for missing keys. 
-    #       Otherwise, even if your agent performs well in training, it may fail during testing.
-
+    state_tensor = torch.FloatTensor(obs).unsqueeze(0)
+    with torch.no_grad():
+        q_values = model(state_tensor)
+    return torch.argmax(q_values).item()
 
     return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
     # You can submit this random agent to evaluate the performance of a purely random strategy.
