9266a26a8c0d37b51e432225084560d92d3fb70b
andy12341210 <andy12341210@gmail.com>
2025-03-18 23:07:33 +0800

    index correction

--- student_agent.py
+++ student_agent.py
@@ -3,6 +3,7 @@ import numpy as np
 import pickle
 import random
 import gym
+import torch
 with open("taxi_dqn_model.pkl", "rb") as f:
  Q_table = pickle.load(f)
 
@@ -12,9 +13,9 @@ def get_action(obs):
     
     state_tensor = torch.FloatTensor(obs).unsqueeze(0)
     with torch.no_grad():
-        q_values = model(state_tensor)
+        q_values = Q_table(state_tensor)
     return torch.argmax(q_values).item()
 
-    return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
+    # return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
     # You can submit this random agent to evaluate the performance of a purely random strategy.
 
